d0bca0b511f3f2fa2e3629cb1bd8ac719f2a735b
Gostavikyi <Gamesnow574@gmail.com>
2024-07-12 20:35:16 -0500

    Update userconfig.js

--- userconfig.js
+++ userconfig.js
@@ -3,20 +3,22 @@ let saved_config = JSON.parse(localStorage.getItem("CONFIG"));
 const default_config = {
   overrideStorage: true,
   temperature: {
-    location: 'Matão, São Paulo',
-    scale: "C",
+    location: 'Chicago, Illinois',
+    scale: "F",
   },
   clock: {
-    format: "h:i p",
+    format: "A ||           d:M: s",
     iconColor: "#ea6962",
   },
   search: {
     engines: {
       g: ["https://google.com/search?q=", "Google"],
-      d: ["https://duckduckgo.com/html?q=", "DuckDuckGo"],
+      e: ["https://e621.net/posts?tags=", "E6"],
       y: ["https://youtube.com/results?search_query=", "Youtube"],
       r: ["https://www.reddit.com/search/?q=", "Reddit"],
       p: ["https://www.pinterest.es/search/pins/?q=", "Pinterest"],
+      a: ["https://www.amazon.com/s?k=", "Amazon"],
+     
     },
   },
   keybindings: {
@@ -25,97 +27,100 @@ const default_config = {
   },
   disabled: [],
   localIcons: false,
-  fastlink: "https://chat.openai.com/",
+  fastlink: "https://omnivore.app/home?q=in%3Ainbox+-label%3Ansfw",
   openLastVisitedTab: true,
   tabs: [
     {
-      name: "chi ll",
-      background_url: "src/img/banners/cbg-2.gif",
+      name: "HOME",
+      background_url: "https://i.pinimg.com/originals/63/54/df/6354dfb863e40a1a159c00ddd5dd7ce4.gif",
       categories: [{
         name: "Social Media",
         links: [
           {
-            name: "whatsapp",
-            url: "https://web.whatsapp.com/",
-            icon: "brand-whatsapp",
+            name: "!tk",
+            url: "https://www.tiktok.com/",
+            icon: "brand-tiktok",
             icon_color: "#a9b665",
           },
+          {name: "p",  url: "https://www.tiktok.com/profile", },
+          {name: "v",  url: "https://www.tiktok.com/live?lang=en", },
           {
-            name: "twitter",
+            name: "!tw",
             url: "https://twitter.com/home",
             icon: "brand-twitter-filled",
             icon_color: "#7daea3",
           },
+  
+
           {
-            name: "reddit",
-            url: "https://www.reddit.com/",
-            icon: "brand-reddit",
+            name: "!bs",
+            url: "https://bsky.app/",
+            icon: "butterfly",
             icon_color: "#e78a4e",
           },
           {
-            name: "youtube",
+            name: "!yt",
             url: "https://www.youtube.com/",
             icon: "brand-youtube-filled",
             icon_color: "#ea6962",
           },
           {
-            name: "twitch",
+            name: "!ttv",
             url: "https://www.twitch.tv/",
             icon: "brand-twitch",
             icon_color: "#d3869b",
           },
+
+          {
+            name: "!ig",
+            url: "https://www.instagram.com/",
+            icon: "brand-instagram",
+            icon_color: "#99858B",
+          },
+
+
+
         ],
       }, {
-        name: "Games",
+        name: "chans",
         links: [
-          {
-            name: "chess",
-            url: "https://www.chess.com/home",
-            icon: "chess-queen-filled",
-            icon_color: "#a9b665",
-          },
-          {
-            name: "monkeytype",
-            url: "https://monkeytype.com/",
-            icon: "keyboard",
-            icon_color: "#e78a4e",
-          },
-          {
-            name: "tetris",
-            url: "https://tetris.com/",
-            icon: "brand-apple-arcade",
-            icon_color: "#ea6962",
-          },
+         
+          {name: "/an/",  url: "https://boards.4chan.org/an/", },
+          {name: "/co/",  url: "https://boards.4chan.org/co/", },
+          {name: "/co/+",  url: "https://boards.plus4chan.org/co/", },
+
+          {name: "/ck/",  url: "https://boards.4chan.org/ck/", },
+          {name: "/v/",  url: "https://boards.4chan.org/m/", },
+          {name: "/tv/",  url: "https://boards.4chan.org/an/", },
+          {name: "/wg/",  url: "https://boards.4chan.org/wg/", },
+          {name: "/wsg/",  url: "https://boards.4chan.org/wsg/", },
+
+
         ],
       }, {
-        name: "Video",
+        name: "Ai✨",
         links: [
           {
-            name: "disney+",
-            url: "https://www.disneyplus.com/home",
-            icon: "brand-disney",
+            name: "Ecos.ai - Client",
+            url: "https://www.ecosia.org/chat?addon=firefox&addonversion=4.1.2&q=s",
+            icon: "plant-2",
             icon_color: "#7daea3",
           },
-          {
-            name: "primevideo",
-            url: "https://www.primevideo.com/region/eu/?ref_=dv_web_unknown",
-            icon: "brand-amazon",
-            icon_color: "#7daea3",
-          },
+       
         ],
       }],
     },
     {
-      name: "design",
+      name: "Media",
       background_url: "src/img/banners/cbg-6.gif",
       categories: [
         {
-          name: "inspiration",
+          name: "Media",
           links: [
             {
-              name: "pinterest",
-              url: "https://www.pinterest.es/",
-              icon: "brand-pinterest",
+              name: "tumblr",
+              url: "https://www.tumblr.com/",
+              icon: "brand-tumblr",
               icon_color: "#ea6962",
             },
             {
@@ -125,73 +130,101 @@ const default_config = {
               icon_color: "#7daea3",
             },
             {
-              name: "leonardo ai",
-              url: "https://app.leonardo.ai/",
+              name: "krea ai",
+              url: "https://www.krea.ai/apps/image/realtime",
               icon: "brand-openai",
               icon_color: "#89b482",
             },
             {
-              name: "dribble",
-              url: "https://dribbble.com/following",
-              icon: "brand-dribbble-filled",
+              name: "saucenao",
+              url: "https://saucenao.com/",
+              icon: "eye-pin",
               icon_color: "#d3869b",
             },
+       
+
           ],
         },
         {
-          name: "resources",
+          name: "Music",
           links: [
             {
-              name: "figma",
-              url: "https://www.figma.com",
-              icon: "brand-figma",
-              icon_color: "#d3869b",
+              name: "spotify",
+              url: "https://open.spotify.com/",
+              icon: "brand-spotify",
+              icon_color: "#63b467",
             },
             {
-              name: "uxpro",
+              name: "soundcloud",
               url: "https://uxpro.cc/",
-              icon: "components",
-              icon_color: "#a9b665",
+              icon: "brand-soundcloud",
+              icon_color: "#d8a07c",
             },
             {
-              name: "colorhunt",
-              url: "https://colorhunt.co/",
-              icon: "color-picker",
+              name: "bandcamp",
+              url: "https://bandcamp.com/",
+              icon: "brand-bandcamp",
               icon_color: "#ea6962",
             },
             {
-              name: "adobe color",
-              url: "https://color.adobe.com/es/create/color-wheel",
+              name: "beatbox",
+              url: "https://choptop84.github.io/abyssbox-app/",
               icon: "brand-adobe",
               icon_color: "#7daea3",
             },
-            {
-              name: "terminalsexy",
-              url: "https://terminal.sexy",
-              icon: "prompt",
-              icon_color: "#e78a4e",
-            },
+      
           ],
         },
         {
-          name: "resources 3d",
+          name: "Streams",
           links: [
-            {
-              name: "thingiverse",
-              url: "https://www.thingiverse.com/",
-              icon: "circle-letter-t",
-              icon_color: "#7daea3",
-            },
+            
+
+            {name: "aniwav",  url: "https://aniwave.to/home", icon: "ripple",
+              icon_color: "#D82051" },
+            {name: "cimcartoon",  url: "https://kimcartoon.li/",  icon: "mickey",
+              icon_color: "#969696", },
+            {name: "fmobies",  url: "https://fmovies24.to/",   icon: "video",
+              icon_color: "#130F10",},
+  
+            {name: "bingged",  url: "https://binged.in/",   icon: "video",
+              icon_color: "#554CA8",},
+            {name: "netflix",  url: "https://www.netflix.com/",  icon: "brand-netflix",
+              icon_color: "#d75e40", },
+            {name: "plex",  url: "https://app.plex.tv/desktop/#!/",  icon: "topology-complex",
+              icon_color: "#ddb839", },
+   
+
+
+
+
           ],
         },
       ],
     },
     {
-      name: "dev",
+      name: "Other",
       background_url: "src/img/banners/cbg-7.gif",
       categories: [
         {
-          name: "repositories",
+          name: "Games",
+          links: [
+            
+            {name: "steamdb",  url: "https://steamdb.info/",  icon: "brand-steam",
+              icon_color: "#777674", },
+   
+              {name: "steamchrt",  url: "https://steamcharts.com/",  icon: "brand-steam",
+                icon_color: "#546B54", },
+
+                {name: "prime stuff",  url: "https://gaming.amazon.com/home",  icon: "ironing-steam",
+                  icon_color: "#796A3B", },
+     
+            
+
+          ],
+        },
+        {
+          name: "resources",
           links: [
             {
               name: "github",
@@ -200,76 +233,91 @@ const default_config = {
               icon_color: "#7daea3",
             },
             {
+              name: "MN",
+              url: "https://github.com/",
+              icon: "home-cog",
+              icon_color: "#6D7070",
+            },
+     
+            {
               name: "gitlab",
               url: "https://gitlab.com/",
               icon: "brand-gitlab",
               icon_color: "#e78a4e",
             },
+
+            {
+              name: "OHB",
+              url: "https://gitlab.com/Gostavikyi/Home/-/blob/master/index.html?ref_type=heads",
+              icon: "home-cog",
+              icon_color: "#6D7070",
+            },
+            {
+              name: "NHB",
+              url: "pastelinkhere",
+              icon: "home-cog",
+              icon_color: "#6D7070",
+            },
+
+            {name: "Priv Megatrd",  url: "https://www.reddit.com/r/Piracy/wiki/megathread/",  icon: "brand-steam",
+              icon_color: "#ddb839", },
+   
+
+            
+     
           ],
         },
         {
-          name: "resources",
+          name: "Other",
           links: [
+   
+        
             {
-              name: "phind",
-              url: "https://www.phind.com/",
-              icon: "brand-openai",
-              icon_color: "#89b482",
-            },
-            {
-              name: "flutter",
-              url: "https://docs.flutter.dev/ui",
-              icon: "brand-flutter",
-              icon_color: "#7daea3",
-            },
-            {
-              name: "hacktricks",
-              url: "https://book.hacktricks.xyz/welcome/readme",
-              icon: "biohazard",
+              name: "colorhunt",
+              url: "https://colorhunt.co/",
+              icon: "color-picker",
               icon_color: "#ea6962",
             },
+
             {
-              name: "vscode",
-              url: "https://vscode.dev/",
-              icon: "brand-vscode",
-              icon_color: "#7daea3",
-            },
-          ],
-        },
-        {
-          name: "challenges",
-          links: [
-            {
-              name: "hackthebox",
-              url: "https://app.hackthebox.com",
-              icon: "box",
-              icon_color: "#a9b665",
-            },
-            {
-              name: "cryptohack",
-              url: "https://cryptohack.org/challenges/",
-              icon: "brain",
+              name: "tmlsexy",
+              url: "https://terminal.sexy",
+              icon: "prompt",
               icon_color: "#e78a4e",
             },
+
             {
-              name: "tryhackme",
-              url: "https://tryhackme.com/dashboard",
-              icon: "brand-onedrive",
-              icon_color: "#ea6962",
+              name: "Tabler",
+              url: "https://tabler.io/icons",
+              icon: "icons",
+              icon_color: "#e78a4e",
             },
+
             {
-              name: "hackerrank",
-              url: "https://www.hackerrank.com/dashboard",
-              icon: "code-asterix",
-              icon_color: "#a9b665",
+              name: "thingi",
+              url: "https://www.thingiverse.com/",
+              icon: "circle-letter-t",
+              icon_color: "#7daea3",
             },
+
+
+            {
+              name: "sketchfab",
+              url: "https://sketchfab.com/",
+              icon: "basket",
+              icon_color: "#7daea3",
+            },
+
+
+
+
           ],
         },
       ],
     },
     {
       name: "myself",
-      background_url: "src/img/banners/cbg-9.gif",
+      background_url: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzBhZTQxZWI2YzJlYzR5emFxcHhrbnFtNXY1ZmVvMWI1emhscTFkbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/CqDTYmxrpApGF9eI6q/giphy.webp",
       categories: [
         {
           name: "mails",
@@ -280,6 +328,31 @@ const default_config = {
               icon: "brand-gmail",
               icon_color: "#ea6962",
             },
+
+            {
+              name: "proton",
+              url: "https://proton.me/",
+              icon: "template",
+              icon_color: "#sa6362",
+            },
+
+            {
+              name: "relay",
+              url: "https://relay.firefox.com/accounts/profile/?",
+              icon: "access-point",
+              icon_color: "#oa6962",
+            },
+
+            {
+              name: "temp",
+              url: "https://temp-mail.org/en/",
+              icon: "brand-ctemplar",
+              icon_color: "#fe6962",
+            },
+
+
+
+
           ],
         },
         {
@@ -292,30 +365,30 @@ const default_config = {
               icon_color: "#e78a4e",
             },
             {
-              name: "dropbox",
-              url: "https://www.dropbox.com/h?role=personal&di=left_nav",
-              icon: "box-seam",
+              name: "mega",
+              url: "https://mega.nz/login",
+              icon: "box",
               icon_color: "#7daea3",
             },
             {
-              name: "fotos",
-              url: "https://photos.google.com/u/1",
-              icon: "photo-filled",
+              name: "catbox",
+              url: "https://catbox.moe/",
+              icon: "cat",
               icon_color: "#ea6962",
             },
+
           ],
         },
+
         {
-          name: "stuff",
+          name: "Personally I think everything is about to go to shit real fast\nSo Why not go out in comfort. Its been 10 years since I started using webpages and so much has happened in those 10 years.\n            ~W.",
           links: [
-            {
-              name: "linkedin",
-              url: "https://www.linkedin.com/feed/",
-              icon: "brand-linkedin",
-              icon_color: "#7daea3",
-            },
+       
+
           ],
         },
+        
+     
       ],
     },
   ],
